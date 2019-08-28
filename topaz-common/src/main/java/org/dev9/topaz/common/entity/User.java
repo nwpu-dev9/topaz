339d13d10a6caf044c021b515c5d15e27e6d006d
TangliziGit <tanglizimail@foxmail.com>
2019-08-28 12:10:44 +0800

    修复保留字user问题

--- topaz-common/src/main/java/org/dev9/topaz/common/entity/User.java
+++ topaz-common/src/main/java/org/dev9/topaz/common/entity/User.java
@@ -7,7 +7,7 @@ import java.util.Date;
 // TODO: shiro: Boolean isAdmin?
 
 @Entity
-@Table(name = "USER")
+@Table(name = "`USER`")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
