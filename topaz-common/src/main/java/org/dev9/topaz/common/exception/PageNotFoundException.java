d47e2961481b354c85fad8f10eff202a80867233
zydiig <me@baka.cafe>
2019-08-28 15:37:01 +0800

    Make exceptions unchecked

--- topaz-common/src/main/java/org/dev9/topaz/common/exception/PageNotFoundException.java
+++ topaz-common/src/main/java/org/dev9/topaz/common/exception/PageNotFoundException.java
@@ -2,7 +2,7 @@ package org.dev9.topaz.common.exception;
 
 import org.springframework.data.domain.PageRequest;
 
-public class PageNotFoundException extends Exception{
+public class PageNotFoundException extends RuntimeException{
     public PageNotFoundException(){
         super();
     }
