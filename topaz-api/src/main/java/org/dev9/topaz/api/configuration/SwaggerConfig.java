7f4dcf19fa8e5b8cbc332e5998d0aedf564c034f
TangliziGit <tanglizimail@foxmail.com>
2019-08-30 09:42:16 +0800

    api: 添加swagger2依赖, 生成api文档

---
+++ topaz-api/src/main/java/org/dev9/topaz/api/configuration/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package org.dev9.topaz.api.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket productApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.dev9.topaz.api"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
